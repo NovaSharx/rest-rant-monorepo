c06f54b32b022ed29cfd73e15c8fbc974d782a72
Davin <davin-dj@hotmail.com>
2023-02-04 12:23:39 -0500

    Completed cookie implementation

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -15,7 +15,33 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password`
         })
     } else {
+        req.session.userId = user.dataValues.userId
         res.json({user})
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    console.log('Session userId: ' + req.session.userId)
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
+// Example of an important route which can be a target for Cross-Site Request Forgery
+router.post('/super-important-route', async (req, res) => {
+    if(req.session.userId) {
+        console.log('Do the really super important sensitive data thing')
+        res.send('Done')
+    } else {
+        console.log('You are not authorized to do the super important sensitive data thing')
+        res.send('Denied')
     }
 })
 
